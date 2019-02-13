962139ccfc50cfd1e238647ccbed2e7328d54da1
shilpisaini <46492461+shilpisaini@users.noreply.github.com>
2019-02-13 23:14:19 +0530

    Update customer.routes.js

--- app/routes/customer.routes.js
+++ app/routes/customer.routes.js
@@ -1,19 +1,21 @@
-module.exports = function(app) {
-
-    var customers = require('../controllers/customer.controller.js');
-
-    // Create a new Customer
-    app.post('/api/customers', customers.create);
-
-    // Retrieve all Customer
-    app.get('/api/customers', customers.findAll);
-
-    // Retrieve a single Customer by Id
-    app.get('/api/customers/:id', customers.findOne);
-
-    // Update a Customer with Id
-    app.put('/api/customers/:id', customers.update);
-
-    // Delete a Customer with Id
-    app.delete('/api/customers/:id', customers.delete);
-}
+let express = require('express');
+let router = express.Router();
+ 
+let customers = require('../controllers/customer.controller.js');
+ 
+// Create a new Customer
+router.post('/api/customers', customers.create);
+ 
+// Retrieve all Customer
+router.get('/api/customers', customers.findAll);
+ 
+// Retrieve a single Customer by Id
+router.get('/api/customers/:id', customers.findOne);
+ 
+// Update a Customer with Id
+router.put('/api/customers', customers.update);
+ 
+// Delete a Customer with Id
+router.delete('/api/customers/:id', customers.delete);
+ 
+module.exports = router;
